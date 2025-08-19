ec633a9de11cb8aad5b08805cb2173285c225f44
deplearning-s <devinasawitrii@gmail.com>
2025-08-19 10:27:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -585,7 +585,7 @@ elif st.session_state.main_tab == 'IPM':
             legend=dict(
                 orientation="h",
                 yanchor="bottom",
-                y=0.5,
+                y=0.9,
                 xanchor="right",
                 x=1,
                 font=dict(size=10)
